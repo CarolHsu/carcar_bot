2a92d9136bfeca57019c83e4b9e21e405ac00fcd
Carol H <sheseee@gmail.com>
2018-03-17 18:19:19 +0800

    Refactor base dialog module

--- lib/dialog/passenger/base.rb
+++ lib/dialog/passenger/base.rb
@@ -1,14 +1,36 @@
 module Dialog
   module Passenger
     class Base
+
+      KEYWORDS = {}
       
-      def initialize(dialog_type)
-        @dialog_type = dialog_type
+      def initialize(passenger, message)
+        @message = message
+        @passenger = passenger
+        get_dialog_type
+        set_dialog_type unless @dialog_type
+        self.start
       end
 
-      def start(event)
+      def start
         dialog = eval("Dialog::Passenger::#{@dialog_type}")
-        dialog.start(event)
+        dialog.start(@message)
+      end
+
+      private
+
+      def get_dialog_type
+        @dialog_type = @passenger.settings.dialog_type
+      end
+
+      def set_dialog_type
+        unless KEYWORDS[@message].nil?
+          @dialog_type = KEYWORDS[@message]
+          @passenger.settings.dialog_type = KEYWORDS[@message]
+        else
+          @dialog_type = "profile"
+          @passenger.settings.dialog_type = "profile"
+        end
       end
 
     end
--- lib/dialog/passenger/profile.rb
+++ lib/dialog/passenger/profile.rb
@@ -4,35 +4,6 @@ module Dialog
   module Passenger
     class Profile
       # SCRIPT = YAML.load_file("#{Rails.root}/lib/dialog/scripts.yml")[:passenger][:call]
-      include AASM
-
-      aasm do
-        state :name, initial: true
-        state :mobile
-        state :email
-      end
-
-      def self.start(event)
-        # pending
-        read_script
-      end
-
-      private
-      def read_script
-        
-      end
-
-      def set_user_email
-        
-      end
-
-      def set_user_mobile
-        
-      end
-
-      def retrieve_user
-        
-      end
     end
   end
 end
