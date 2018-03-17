86a81533257c4e9673302876c87b765b7395bb6f
Carol H <sheseee@gmail.com>
2018-03-17 18:17:52 +0800

    Add basic state machine for passenger model (and settings cached

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,2 +1,18 @@
 class Passenger < ApplicationRecord
+  include RailsSettings::Extend
+  include AASM
+
+  aasm do
+    state :inactive, initial: true
+    state :no_name
+    state :ready
+
+    event :get_phone do
+      transitions from: :inactive, to: :no_name
+    end
+
+    event :get_name do
+      transitions from: :no_name, to: :ready
+    end
+  end
 end
